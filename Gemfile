418787bc53fbbfffdb22010488a909fae101d7e0
Ronald Tse <ronald.tse@ribose.com>
2024-06-28 20:13:48 +0800

    feat: initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem "sinatra"
+gem "puma"
+gem "httparty"
+gem "rackup", "~> 2.1"
---
+++ app.rb
@@ -0,0 +1,12 @@
+require "sinatra"
+require "httparty"
+
+get "/" do
+  "Welcome to the Tebako App on macOS!"
+end
+
+get "/api" do
+  content_type :json
+  response = HTTParty.get("https://jsonplaceholder.typicode.com/todos/1")
+  response.body
+end
