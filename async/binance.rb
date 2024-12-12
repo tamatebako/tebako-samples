bcec40edc9b250c4287354abdffc6ccbfe1950d9
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2024-12-13 00:15:45 +0300

    feat: create async gem sample

---
+++ async/binance.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Released under the MIT License.
+# Copyright, 2021-2024, by Samuel Williams.
+
+require "async"
+require "async/http"
+require "async/websocket"
+
+URL = "wss://stream.binance.com:9443/ws/btcusdt@bookTicker"
+
+Async do |task|
+	endpoint = Async::HTTP::Endpoint.parse(URL, alpn_protocols: Async::HTTP::Protocol::HTTP11.names)
+
+	Async::WebSocket::Client.connect(endpoint) do |connection|
+		while message = connection.read
+			$stdout.puts message.parse
+		end
+	end
+end
